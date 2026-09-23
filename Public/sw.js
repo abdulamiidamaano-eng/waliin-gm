ffa17cb608dd80b25ee9ee3bd26fde0736b137c1
abdulamiidamaano-eng <abdulamiidamaano@gmail.com>
2026-09-23 21:08:15 +0300

    Update sw.js

--- Public/sw.js
+++ Public/sw.js
@@ -1,11 +1,35 @@
-const CACHE_NAME = "waliin-gm-v2";
+const CACHE_NAME = "waliin-gm-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
 
 self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
